cb53c5deb686b5deea48be9ded2d549bc5240e15
Okan Ataç <77567379+mommymephala@users.noreply.github.com>
2023-06-16 00:47:42 +0300

    Initial commit
    Created the GridManager and imported other visual assets.
    
    Grid scales with 16:9 aspect ratio and positioned correctly.

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    const int numRows = 10;
+    const int numColumns = 10;
+    public GameObject cellPrefab;
+
+    void Start()
+    {
+        // change aspect wrto screen size, 1080p
+        Camera.main.aspect = 1920f / 1080f; 
+        GenerateGrid();
+    }
+
+    void GenerateGrid()
+    {
+        Vector2 cellSize = cellPrefab.GetComponent<SpriteRenderer>().bounds.size;
+
+        float gridWidth = numColumns * cellSize.x;
+        float gridHeight = numRows * cellSize.y;
+
+        float scaleFactor = 1f;
+
+        if (gridWidth > Screen.width || gridHeight > Screen.height)
+        {
+            float scaleX = Screen.width / gridWidth;
+            float scaleY = Screen.height / gridHeight;
+            scaleFactor = Mathf.Min(scaleX, scaleY);
+        }
+
+        float scaledCellWidth = cellSize.x * scaleFactor;
+        float scaledCellHeight = cellSize.y * scaleFactor;
+       
+
+        float spacingX = scaledCellWidth; 
+        float spacingY = scaledCellHeight;
+
+        float startX = -(gridWidth / 2f) + (scaledCellWidth / 2f);
+        float startY = (gridHeight / 2f) - (scaledCellHeight / 2f);
+
+        Debug.Log(startX);
+
+        for (int row = 0; row < numRows; row++)
+        {   
+            for (int col = 0; col < numColumns; col++)
+            {
+                // Calculate the position of each cell based on row and column values
+                Vector2 position = new Vector2(col * spacingX, row * spacingY);
+
+                float rightAlignOffset = (Camera.main.orthographicSize / 2f * Camera.main.aspect) - (scaledCellWidth / 2f);
+                
+                // Apply the starting offset
+                position.x += startX + rightAlignOffset;
+                position.y -= startY;
+
+
+                // Instantiate a cell GameObject at the calculated position
+                GameObject cell = Instantiate(cellPrefab, position, Quaternion.identity);
+
+                // Set the parent of the cell GameObject to the grid container
+                cell.transform.SetParent(transform);
+
+            }
+        }
+    }
+}
